a727d1d7c1861c7ebc29a846ff78d479b38ac1a4
Markus Binsteiner <m.binsteiner@auckland.ac.nz>
2012-07-09 11:20:17 +1200

    Fixed windows path

--- grisu-template/windows/grisu-template.bat
+++ grisu-template/windows/grisu-template.bat
@@ -1,2 +1,9 @@
-
-java -jar "getdown-client.jar" "%APPDATA%\Grisu\Grisu Template Client"
+if NOT EXIST "%APPDATA%\Grid\Grisu Template Client\getdown.txt" (
+	MD "%APPDATA%\Grid\Grisu Template Client\"
+	COPY ..\resources\getdown.txt "%APPDATA%\Grid\Grisu Template Client\getdown.txt"
+)
+if NOT EXIST "%APPDATA%\Grid\Grisu Template Client\splash.png" (
+	MD "%APPDATA%\Grid\Grisu Template Client\"
+	COPY ..\resources\splash.png "%APPDATA%\Grid\Grisu Template Client\splash.png"
+)
+java -jar "getdown-client.jar" "%APPDATA%\Grid\Grisu Template Client"
--- izpack/windows/uninstall.vbs
+++ izpack/windows/uninstall.vbs
@@ -1,4 +1,4 @@
 Set oShell = CreateObject ("Wscript.Shell") 
 Dim strArgs
-strArgs = "cmd /c grisu-template-uninstall.bat"
+strArgs = "cmd /c uninstall.bat"
 oShell.Run strArgs, 0, false
