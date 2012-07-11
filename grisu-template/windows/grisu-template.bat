8609401f9d4ed788369185fe39a34a3f23095f5b
Markus Binsteiner <m.binsteiner@auckland.ac.nz>
2012-07-11 15:22:08 +1200

    Fixing windows for non-standard java install

--- grisu-template/windows/grisu-template.bat
+++ grisu-template/windows/grisu-template.bat
@@ -1,5 +1,6 @@
-set dir="%APPDATA%\Grid\GrisuTemplateClient"
+@ECHO OFF
 
+set dir=%APPDATA%\Grid\GrisuTemplateClient
 
 if NOT EXIST "%dir%\getdown.txt" (
 	MD "%dir%"
@@ -9,4 +10,24 @@ if NOT EXIST "%dir%\splash.png" (
 	MD "%dir%"
 	COPY ..\resources\splash.png "%dir%\splash.png"
 )
-java -jar "getdown-client.jar" "%dir%"
+
+
+FOR /F "skip=2 tokens=2*" %%A IN ('REG QUERY "HKLM\Software\JavaSoft\Java Runtime Environment" /v CurrentVersion') DO set CurVer=%%B
+
+FOR /F "skip=2 tokens=2*" %%A IN ('REG QUERY "HKLM\Software\JavaSoft\Java Runtime Environment\%CurVer%" /v JavaHome') DO set JAVA_HOME=%%B
+
+REM @echo JAVA_HOME = %JAVA_HOME%
+set JAVA_EXE=%JAVA_HOME%\bin\java.exe
+
+@echo Using Java: %JAVA_EXE%
+if NOT EXIST "%JAVA_EXE%" (
+
+	ECHO WScript.Echo( "Can't find Java executable." ^& vbCrLf ^& vbCrLf ^& "Please contact support:" ^& vbCrLf ^& vbCrLf ^& "m.binsteiner@auckland.ac.nz" ^)  > usermessage.vbs
+	WSCRIPT.EXE usermessage.vbs
+	DEL usermessage.vbs
+
+)
+
+
+"%JAVA_EXE%" -jar "getdown-client.jar" "%dir%"
+
